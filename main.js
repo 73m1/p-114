620674f163c545bbae6611ebf484e6d70688c559
73m1 <87555478+73m1@users.noreply.github.com>
2022-12-11 14:03:16 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+function setup() {
+canvas = createCanvas(600,600);
+canvas.center();
+video = createCapture(600,600)
+}
+
+function draw() {
+    
+}
+
+function snap() {
+    save("InstantMoustache.png");
+}
